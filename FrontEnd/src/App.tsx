704074f12134bfaf124764aaa7103a9869f12dc2
Cat Tuong <116152919+TuongNguyen13@users.noreply.github.com>
2025-11-05 17:46:38 +0700

    Thêm trang employee

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,34 +1,27 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import ReactDom from 'react-dom/client'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
 import './App.css'
+import Login from './Components/Login'
+import Dashboard from './Components/Dashboard'
+import Order from './Components/Order'
+import AddOrder from './Components/AddOrder'
+import Employee from './Components/Employee'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Routes>
+        <Route path='/' element ={<Login/>}/>
+        <Route path='/dashboard' element ={<Dashboard/>}/>
+        <Route path='/order' element={<Order/>}/>
+        <Route path='/order/add-order' element= {<AddOrder/>}/>
+        <Route path='/employee' element = {<Employee/>}/>
+      </Routes>
+    </div>
   )
 }
 
---
+++ FrontEnd/src/Components/Employee.tsx
@@ -0,0 +1,122 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import '../styles/employee.css'
+
+
+
+function Employee() {
+    const [showModal, setShowModal] = useState(false);
+
+    useEffect(() => {
+        document.title = "Nhân viên";
+
+    }, [])
+
+    const handleAddEmployee = () => {
+        setShowModal(true)
+    }
+
+    const handleCancelEmployee = () => {
+        setShowModal(false)
+    }
+
+    return (
+        <div className="employee-container">
+            <div className="employee-header">
+                <h2>Quản lý nhân viên</h2>
+                <div className="employee-search">
+                    <input type="text" placeholder="Tìm kiếm..." />
+                    <button>Tìm kiếm</button>
+                </div>
+                <div>
+                    <button onClick={() => handleAddEmployee()}>Thêm nhân viên</button>
+                </div>
+                <div className="employee-table-information">
+                    <table>
+                        <caption>Thông tin nhân viên</caption>
+                        <thead>
+                            <tr>
+                                <th>Mã nhân viên</th>
+                                <th>Tên nhân viên</th>
+                                <th>Giới tính</th>
+                                <th>Email</th>
+                                <th>Chức năng</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+
+                        </tbody>
+
+                    </table>
+                </div>
+
+                {/* phân trang */}
+                <div>
+                    <button>Trang trước</button>
+                    <span></span>
+                    <button>Trang sau</button>
+                </div>
+                {/*Thêm - sửa nhân viên */}
+                <div>
+                    {showModal && (
+                        <div>
+                            <div>
+                                <h2>{ }</h2>
+                                <form action="#">
+                                    <div>
+                                        <label htmlFor="emplyee-code">Mã nhân viên</label>
+                                        <input type="text" disabled />
+                                    </div>
+
+                                    <div>
+                                        <label htmlFor="employee-name">Tên nhân viên</label>
+                                        <input type="text" placeholder="Nhập tên nhân viên" name="employee-name" />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="employee-gender">Giới tính</label>
+                                        <div>
+                                            
+                                                <span>Nam </span>
+                                                <input type="radio" name="employee-gender" value="male" />
+    
+                                                <span> Nữ </span>
+                                                <input type="radio" name="employee-gender" value="female" />
+                                        </div>
+
+                                    </div>
+                                    <div>
+                                        <label htmlFor="employee-birthday">Ngày sinh</label>
+                                        <input type="date" name="employee-birthday" placeholder="Nhập ngày sinh" />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="employee-email">Email</label>
+                                        <input type="text" placeholder="Nhập email..." name="employee-email" />
+                                    </div>
+                                    <div>
+                                        <select name="employee-grant" id="txt-employee-grant">
+                                            <option value="normal-employee" defaultChecked>Nhân viên</option>
+                                            <option value="managa-employee">Quản lý</option>
+                                            <option value="admin-employee">Admin</option>
+                                        </select>
+                                    </div>
+                                    <div>
+                                        <label htmlFor="employee-address">Địa chỉ</label>
+                                        <input type="text" placeholder="Nhập địa chỉ nhân viên..." />
+                                    </div>
+                                    <div>
+                                        <button type="submit">Thêm nhân viên</button>
+                                        <button onClick={()=>handleCancelEmployee()}>Cancel</button>
+                                    </div>
+
+                                </form>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Employee;
